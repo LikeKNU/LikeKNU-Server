99b75d955b0fcc17cf9f514427dffeb949c25be9
jcw1031 <jcw001031@gmail.com>
2023-09-14 18:54:43 +0900

    initial project

---
+++ src/test/java/ac/knu/likeknu/LikeKnuApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.knu.likeknu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeKnuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
