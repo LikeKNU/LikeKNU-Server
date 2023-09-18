b035a974e1ad4f6cb10d919a13f320865c7adf6c
ahma0 <dsd932@naver.com>
2023-09-18 16:05:41 +0900

    Fix: 코드리뷰에 따라 수정

--- src/main/java/ac/knu/likeknu/service/MainService.java
+++ src/main/java/ac/knu/likeknu/service/MainService.java
@@ -6,13 +6,15 @@ import ac.knu.likeknu.domain.Campus;
 import ac.knu.likeknu.domain.Tag;
 import ac.knu.likeknu.repository.AnnouncementRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MainService {
@@ -20,20 +22,17 @@ public class MainService {
     private final AnnouncementRepository announcementRepository;
 
     public ResponseEntity<List<MainAnnouncementsResponse>> getAnnouncementsResponse(Campus campus) {
-        List<Campus> campusList = new ArrayList<>();
-        campusList.add(Campus.ALL);
-        campusList.add(campus);
+        List<Campus> campusList = List.of(Campus.ALL, campus);
 
-        List<Announcement> getAnnouncements =
+        Optional<List<Announcement>> getAnnouncementsOptinal =
                 announcementRepository
-                        .findTop4ByCampusInAndTagOrderByAnnouncementDateDesc(campusList, Tag.SCHOOL_NEWS)
-                        .orElse(null);
+                        .findTop4ByCampusInAndTagOrderByAnnouncementDateDesc(campusList, Tag.SCHOOL_NEWS);
 
-        if(getAnnouncements == null)
-            throw new NullPointerException("null 값이 반환되었습니다.");
+        if(getAnnouncementsOptinal.isEmpty())
+            log.info("게시물이 없습니다.");
 
         return ResponseEntity.ok(
-                getAnnouncements.stream()
+                getAnnouncementsOptinal.get().stream()
                         .map((Announcement a) -> MainAnnouncementsResponse.of(a))
                         .collect(Collectors.toList())
         );
