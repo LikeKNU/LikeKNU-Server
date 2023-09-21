cbf860a9bf206fb3037d0d3e1342a2e71690b6d6
jcw1031 <jcw001031@gmail.com>
2023-09-21 22:18:40 +0900

    Fix: BaseEntity 제거

--- src/main/java/ac/knu/likeknu/domain/Menu.java
+++ src/main/java/ac/knu/likeknu/domain/Menu.java
@@ -1,12 +1,12 @@
 package ac.knu.likeknu.domain;
 
 import ac.knu.likeknu.domain.value.Campus;
-import ac.knu.likeknu.domain.value.Domain;
 import ac.knu.likeknu.domain.value.MealType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,7 +14,10 @@ import java.time.LocalDate;
 
 @Getter
 @Entity
-public class Menu extends BaseEntity {
+public class Menu {
+
+    @Id
+    private String id;
 
     @Column
     private String menus;
@@ -32,16 +35,14 @@ public class Menu extends BaseEntity {
     private Cafeteria cafeteria;
 
     protected Menu() {
-        super(Domain.MENU);
     }
 
     @Builder
     public Menu(String menus, MealType mealType, LocalDate date, Campus campus, Cafeteria cafeteria) {
-        this();
         this.menus = menus;
         this.mealType = mealType;
         this.date = date;
         this.campus = campus;
-        this.cafeteria =  cafeteria;
+        this.cafeteria = cafeteria;
     }
 }
