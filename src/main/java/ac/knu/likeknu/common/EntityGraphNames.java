d2e04086ad9dc68247455551b14273969894aa00
jcw1031 <jcw001031@gmail.com>
2023-09-25 00:05:14 +0900

    Fix: 사용하지 않 엔티티 그래프 이름 제거

--- src/main/java/ac/knu/likeknu/common/EntityGraphNames.java
+++ src/main/java/ac/knu/likeknu/common/EntityGraphNames.java
@@ -3,5 +3,4 @@ package ac.knu.likeknu.common;
 public class EntityGraphNames {
 
     public static final String BUS_ARRIVAL_TIMES = "bus.arrivalTimes";
-    public static final String ROUTE_BUSES_ARRIVAL_TIMES = "route.buses.arrivalTimes";
 }
