61ba889f59d7c749b96bdb4ae5034aed1f9f48a3
jcw1031 <jcw001031@gmail.com>
2023-09-24 18:27:12 +0900

    Feat: Spring Data JPA 설정

--- src/main/java/ac/knu/likeknu/common/EntityGraphNames.java
+++ src/main/java/ac/knu/likeknu/common/EntityGraphNames.java
@@ -3,4 +3,5 @@ package ac.knu.likeknu.common;
 public class EntityGraphNames {
 
     public static final String BUS_ARRIVAL_TIMES = "bus.arrivalTimes";
+    public static final String ROUTE_BUSES_ARRIVAL_TIMES = "route.buses.arrivalTimes";
 }
---
+++ src/main/java/ac/knu/likeknu/repository/CityBusRepository.java
@@ -0,0 +1,7 @@
+package ac.knu.likeknu.repository;
+
+import ac.knu.likeknu.domain.CityBus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityBusRepository extends JpaRepository<CityBus, String> {
+}
