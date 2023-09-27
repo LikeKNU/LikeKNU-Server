dbd529e06316e3202392feb0dc83f258d4087c0d
jcw1031 <jcw001031@gmail.com>
2023-09-27 23:47:15 +0900

    Feat: Page 응답 DTO 추가

---
+++ src/main/java/ac/knu/likeknu/controller/dto/response/PageDto.java
@@ -0,0 +1,21 @@
+package ac.knu.likeknu.controller.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class PageDto {
+
+    private final int currentPage;
+    private int totalPages;
+
+    protected PageDto(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public static PageDto of(int currentPage) {
+        if (currentPage < 1) {
+            //TODO Custom exception
+        }
+        return new PageDto(currentPage);
+    }
+}
