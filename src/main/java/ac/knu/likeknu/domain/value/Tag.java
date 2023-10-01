8006f5a1e4b37ed67fdcf4d4516107a20c0950dd
jcw1031 <jcw001031@gmail.com>
2023-10-02 00:55:38 +0900

    Fix: Tag 열거형 상수 정의

--- src/main/java/ac/knu/likeknu/domain/value/Tag.java
+++ src/main/java/ac/knu/likeknu/domain/value/Tag.java
@@ -5,6 +5,20 @@ import lombok.Getter;
 @Getter
 public enum Tag {
 
-    //test용 enum
-    Test;
+    ENROLMENT("수강신청"),
+    DORMITORY("생활관"),
+    SCHOLARSHIP("장학금"),
+    TUITION("등록금"),
+    WORK("근로"),
+    MILEAGE("마일리지"),
+    FIELD_TRAINING("현장실습"),
+    LIBRARY("도서관"),
+    TALENT_DEVELOPMENT("인재개발"),
+    ETC("기타");
+
+    private final String tagName;
+
+    Tag(String tagName) {
+        this.tagName = tagName;
+    }
 }
