dae52c4c17e00d564b2a1330fad8a6959327b9ca
jcw1031 <jcw001031@gmail.com>
2023-10-06 00:14:37 +0900

    Fix: LocalTime 비교 Comparator 커스텀

--- src/test/java/ac/knu/likeknu/service/CityBusServiceTest.java
+++ src/test/java/ac/knu/likeknu/service/CityBusServiceTest.java
@@ -23,6 +23,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatList;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -53,7 +54,7 @@ class CityBusServiceTest {
         CityBus cityBus4 = TestInstanceFactory.createCityBus("130");
         CityBus cityBus5 = TestInstanceFactory.createCityBus("140");
 
-        when(routeRepository.findByCampus(Campus.CHEONAN, Sort.unsorted()))
+        when(routeRepository.findByCampus(eq(Campus.CHEONAN), any(Sort.class)))
                 .thenReturn(List.of(route1, route2, route3));
         when(cityBusRepository.findByRoutesContaining(eq(route1)))
                 .thenReturn(List.of(cityBus1, cityBus3));
