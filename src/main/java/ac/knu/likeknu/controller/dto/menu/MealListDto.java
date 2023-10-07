e9d88d904e8785cf45f7477a212a5dae1b8548f3
ahma0 <dsd932@naver.com>
2023-10-07 19:11:48 +0900

    Feat: 메뉴 상세조회 dto 수정

---
+++ src/main/java/ac/knu/likeknu/controller/dto/menu/MealListDto.java
@@ -0,0 +1,24 @@
+package ac.knu.likeknu.controller.dto.menu;
+
+import ac.knu.likeknu.domain.Cafeteria;
+import ac.knu.likeknu.domain.value.MealType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class MealListDto {
+
+    private MealType mealType;
+    private String operatingTime;
+    private List<MenuListDto> menus;
+
+    @Builder
+    public MealListDto(MealType mealType, String operatingTime, List<MenuListDto> menus) {
+        this.mealType = mealType;
+        this.operatingTime = operatingTime;
+        this.menus = menus;
+    }
+
+}
--- src/main/java/ac/knu/likeknu/controller/dto/menu/MenuResponse.java
+++ src/main/java/ac/knu/likeknu/controller/dto/menu/MenuResponse.java
@@ -15,44 +15,26 @@ public class MenuResponse {
 
     private String cafeteriaId;
     private CafeteriaName cafeteriaName;
-    private MealType cafeteriaMealType;
-    private String cafeteriaTime;
-    private List<MenuListDto> menus;
+    private List<MealListDto> meal;
 
     @Builder
-    public MenuResponse(String cafeteriaId, CafeteriaName cafeteriaName, MealType cafeteriaMealType, String cafeteriaTime, List<MenuListDto> menus) {
+    public MenuResponse(String cafeteriaId, CafeteriaName cafeteriaName) {
         this.cafeteriaId = cafeteriaId;
         this.cafeteriaName = cafeteriaName;
-        this.cafeteriaMealType = cafeteriaMealType;
-        this.cafeteriaTime = cafeteriaTime;
-        this.menus = menus;
     }
 
-    public static MenuResponse of(Cafeteria cafeteria, Menu menu) {
-        List<MenuListDto> menuList = new ArrayList<>();
-
-        if (menu.getMenus() != null){
-            String[] menus = menu.getMenus().split(" ");
-            for (int i = 0; i < menus.length; i++) {
-                menuList.add(MenuListDto.of(i + 1, menus[i]));
-            }
-        }
+    public static MenuResponse of(Cafeteria cafeteria) {
 
         return MenuResponse.builder()
                 .cafeteriaId(cafeteria.getId())
                 .cafeteriaName(cafeteria.getCafeteriaName())
-                .cafeteriaMealType(menu.getMealType())
-                .cafeteriaTime(cafeteria.getTime(menu.getMealType()))
-                .menus(menuList)
                 .build();
     }
 
-    public static MenuResponse empty(Cafeteria cafeteria, MealType mealType) {
+    public static MenuResponse empty(Cafeteria cafeteria) {
         return MenuResponse.builder()
                 .cafeteriaId(cafeteria.getId())
                 .cafeteriaName(cafeteria.getCafeteriaName())
-                .cafeteriaMealType(mealType)
-                .cafeteriaTime(cafeteria.getTime(mealType))
                 .build();
     }
 }
