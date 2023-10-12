11f7691bbaa4f66152a783e2a7ce4d41613417b5
ahma0 <dsd932@naver.com>
2023-10-12 16:54:50 +0900

    Test: 식단 상세 api 캠퍼스별로 수정

--- src/test/java/ac/knu/likeknu/controller/MenuControllerTest.java
+++ src/test/java/ac/knu/likeknu/controller/MenuControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -34,7 +35,7 @@ public class MenuControllerTest {
     @MockBean
     private MenuService menuService;
 
-    @DisplayName("메뉴 상세조회 API 요청에 성공한다.")
+    @DisplayName("캠퍼스별 메뉴 상세조회 API 요청에 성공한다.")
     @Test
     void getMenuResponsesAndSuccess() throws Exception {
         //given
@@ -74,27 +75,55 @@ public class MenuControllerTest {
                 )
         );
 
-        List<MenuResponse> menuResponseList = List.of(menuResponse1, menuResponse2, menuResponse3);
+        MenuResponse menuResponse4 = new MenuResponse(
+                "Test3",
+                "TestName3",
+                List.of(
+                        new MealListDto("아침", "TestTime6", menuListDtos),
+                        new MealListDto("점심", "TestTime7", menuListDtos),
+                        new MealListDto("저녁", "TestTime8", menuListDtos)
+                )
+        );
+
+        List<MenuResponse> menuResponseList1 = List.of(menuResponse1, menuResponse2, menuResponse4);
+        List<MenuResponse> menuResponseList2 = List.of(menuResponse1, menuResponse3, menuResponse4);
 
         //when
-        when(menuService.getMenuResponsesByCampus(Campus.CHEONAN))
-                .thenReturn(menuResponseList);
+        when(menuService.getMenuResponsesByCampus(eq(Campus.CHEONAN)))
+                .thenReturn(menuResponseList1);
+        when(menuService.getMenuResponsesByCampus(eq(Campus.SINGWAN)))
+                .thenReturn(menuResponseList2);
 
-        ResultActions resultActions = mockMvc.perform(
+        ResultActions resultActions1 = mockMvc.perform(
                 get("/api/menu")
                         .param("campus", Campus.CHEONAN.name())
         );
+        ResultActions resultActions2 = mockMvc.perform(
+                get("/api/menu")
+                        .param("campus", Campus.SINGWAN.name())
+        );
 
         //then
-        resultActions.andExpectAll(
+        resultActions1.andExpectAll(
                 status().isOk(),
                 jsonPath("$.data.body.[0].cafeteriaId").value(menuResponse1.getCafeteriaId()),
                 jsonPath("$.data.body.[1].cafeteriaName").value(menuResponse2.getCafeteriaName()),
-                jsonPath("$.data.body.[2].meal.[0].mealType").value(menuResponse3.getMeal().get(0).getMealType()),
-                jsonPath("$.data.body.[2].meal.[1].operatingTime").value(menuResponse3.getMeal().get(1).getOperatingTime()),
+                jsonPath("$.data.body.[2].meal.[0].mealType").value(menuResponse4.getMeal().get(0).getMealType()),
+                jsonPath("$.data.body.[2].meal.[1].operatingTime").value(menuResponse4.getMeal().get(1).getOperatingTime()),
                 jsonPath("$.data.body.[1].meal.[2].menus").value(menuResponse2.getMeal().get(2).getMenus()),
-                jsonPath("$.data.body.[2].meal.[2].menus.[1].menuId").value(menuResponse3.getMeal().get(2).getMenus().get(1).getMenuId()),
-                jsonPath("$.data.body.[2].meal.[2].menus.[2].menuName").value(menuResponse3.getMeal().get(2).getMenus().get(2).getMenuName())
+                jsonPath("$.data.body.[2].meal.[2].menus.[1].menuId").value(menuResponse4.getMeal().get(2).getMenus().get(1).getMenuId()),
+                jsonPath("$.data.body.[2].meal.[2].menus.[2].menuName").value(menuResponse4.getMeal().get(2).getMenus().get(2).getMenuName())
+        ).andDo(print());
+
+        resultActions2.andExpectAll(
+                status().isOk(),
+                jsonPath("$.data.body.[0].cafeteriaId").value(menuResponse1.getCafeteriaId()),
+                jsonPath("$.data.body.[1].cafeteriaName").value(menuResponse3.getCafeteriaName()),
+                jsonPath("$.data.body.[2].meal.[0].mealType").value(menuResponse4.getMeal().get(0).getMealType()),
+                jsonPath("$.data.body.[2].meal.[1].operatingTime").value(menuResponse4.getMeal().get(1).getOperatingTime()),
+                jsonPath("$.data.body.[1].meal.[2].menus.[0].menuName").value(menuResponse3.getMeal().get(2).getMenus().get(0).getMenuName()),
+                jsonPath("$.data.body.[2].meal.[2].menus.[1].menuId").value(menuResponse4.getMeal().get(2).getMenus().get(1).getMenuId()),
+                jsonPath("$.data.body.[2].meal.[2].menus.[2].menuName").value(menuResponse4.getMeal().get(2).getMenus().get(2).getMenuName())
         ).andDo(print());
     }
 }
