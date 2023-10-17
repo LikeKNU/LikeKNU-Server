1efae682d0f287a7fd85147aca3334edd27f7eba
ahma0 <dsd932@naver.com>
2023-10-17 20:52:19 +0900

    Feat: initBinder 제거

--- src/main/java/ac/knu/likeknu/controller/ScheduleController.java
+++ src/main/java/ac/knu/likeknu/controller/ScheduleController.java
@@ -4,8 +4,6 @@ import ac.knu.likeknu.controller.dto.base.ResponseDto;
 import ac.knu.likeknu.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.propertyeditors.CustomNumberEditor;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -23,26 +21,16 @@ public class ScheduleController {
             @RequestParam(name = "year", defaultValue = "year") Integer year,
             @RequestParam(name = "month", defaultValue = "month") Integer month
     ) {
+        if(areYearAndMonthNull(year, month)) {
+            year = LocalDate.now().getYear();
+            month = LocalDate.now().getMonthValue();
+        }
+
         return ResponseDto.of(null);
     }
 
-    @InitBinder
-    private void InitBinder(WebDataBinder binder) throws Exception {
-        CustomNumberEditor customNumberEditor = new CustomNumberEditor(Integer.class, true) {
-
-            @Override
-            public void setAsText(String text) throws IllegalArgumentException {
-                if ("year".equals(text)) {
-                    setValue(LocalDate.now().getYear());
-                } else if("month".equals(text)) {
-                    setValue(LocalDate.now().getMonthValue());
-                } else {
-                    super.setAsText(text);
-                }
-            }
-        };
-
-        binder.registerCustomEditor(Integer.class, customNumberEditor);
+    private boolean areYearAndMonthNull(Integer year, Integer month) {
+        return year == null || month == null;
     }
 
 }
