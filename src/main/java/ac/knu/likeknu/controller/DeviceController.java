0ae83d0cb07401ae8c8362cb3474683fe3f4093f
ahma0 <dsd932@naver.com>
2023-11-02 23:54:33 +0900

    Feat: CampusModificationRequest 생성 및 컨트롤러 추가

--- src/main/java/ac/knu/likeknu/controller/DeviceController.java
+++ src/main/java/ac/knu/likeknu/controller/DeviceController.java
@@ -1,14 +1,12 @@
 package ac.knu.likeknu.controller;
 
 import ac.knu.likeknu.controller.dto.base.ResponseDto;
+import ac.knu.likeknu.controller.dto.device.request.CampusModificationRequest;
 import ac.knu.likeknu.controller.dto.device.request.DeviceRegistrationRequest;
 import ac.knu.likeknu.service.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/devices")
@@ -21,6 +19,11 @@ public class DeviceController {
     public ResponseDto<String> registerDevice(@RequestBody DeviceRegistrationRequest request) {
         ResponseEntity<String> responseEntity = deviceService.registerDeviceId(request);
         return ResponseDto.of(responseEntity.getBody());
+    }
+
+    @PutMapping("/campus")
+    public ResponseDto<String> modifyCampus(@RequestBody CampusModificationRequest request) {
+        return ResponseDto.of("Campus has been changed successfully.");
     }
 
 }
