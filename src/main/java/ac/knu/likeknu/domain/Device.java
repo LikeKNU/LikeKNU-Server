4393c231783db0eaf992a388ff6892c7df7ec454
ahma0 <dsd932@naver.com>
2023-11-09 17:11:56 +0900

    Fix: merge중 삭제된 코드 추가

--- src/main/java/ac/knu/likeknu/domain/Device.java
+++ src/main/java/ac/knu/likeknu/domain/Device.java
@@ -2,9 +2,22 @@ package ac.knu.likeknu.domain;
 
 import ac.knu.likeknu.controller.dto.device.request.DeviceRegistrationRequest;
 import ac.knu.likeknu.domain.value.Campus;
-import jakarta.persistence.*;
+import ac.knu.likeknu.domain.value.Tag;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -21,6 +34,7 @@ public class Device {
     @Column(unique = true)
     private String fcmToken;
 
+    @Setter
     @Enumerated(value = EnumType.STRING)
     @Column(nullable = false)
     private Campus campus;
@@ -33,6 +47,13 @@ public class Device {
             inverseJoinColumns = @JoinColumn(name = "notification_id"))
     @ManyToMany
     private List<Notification> notifications = new ArrayList<>();
+
+    @Enumerated(value = EnumType.STRING)
+    @CollectionTable(name = "subscribe", joinColumns = @JoinColumn(name = "device_id"))
+    @Column(name = "tag")
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<Tag> subscribeTags = new ArrayList<>();
+
 
     protected Device() {
     }
@@ -51,5 +72,10 @@ public class Device {
                 .campus(Campus.SINGWAN)
                 .registeredAt(LocalDateTime.now())
                 .build();
+    }
+
+    public void updateSubscribesTags(List<Tag> tags) {
+        subscribeTags.clear();
+        subscribeTags.addAll(tags);
     }
 }
