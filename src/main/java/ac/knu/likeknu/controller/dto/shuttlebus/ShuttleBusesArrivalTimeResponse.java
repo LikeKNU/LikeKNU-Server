a4aab2a333973b133a671789ad3b6dfd74c07bd2
jcw1031 <jcw001031@gmail.com>
2023-11-19 21:52:57 +0900

    feat: 셔틀버스 운행 여부 추가

--- src/main/java/ac/knu/likeknu/controller/dto/shuttlebus/ShuttleBusesArrivalTimeResponse.java
+++ src/main/java/ac/knu/likeknu/controller/dto/shuttlebus/ShuttleBusesArrivalTimeResponse.java
@@ -4,21 +4,29 @@ import ac.knu.likeknu.domain.ShuttleBus;
 import ac.knu.likeknu.domain.ShuttleTime;
 import lombok.Builder;
 
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 
 @Builder
-public record ShuttleBusesArrivalTimeResponse(String busName, List<ShuttleTimeDto> times) {
+public record ShuttleBusesArrivalTimeResponse(String busName, boolean isRunning, List<ShuttleTimeDto> times) {
 
     public static ShuttleBusesArrivalTimeResponse of(ShuttleBus shuttleBus) {
-        List<ShuttleTimeDto> shuttleTimeList = shuttleBus.getShuttleTimes()
-                .stream()
+        List<ShuttleTime> shuttleTimes = shuttleBus.getShuttleTimes();
+        List<ShuttleTimeDto> shuttleTimeList = shuttleTimes.stream()
                 .sorted(Comparator.comparing(ShuttleTime::getArrivalTime))
                 .map(ShuttleTimeDto::of)
                 .toList();
+
+        boolean isRunning = true;
+        ShuttleTime lastTime = shuttleTimes.get(shuttleTimes.size() - 1);
+        if (LocalTime.now().isAfter(lastTime.getArrivalTime())) {
+            isRunning = false;
+        }
         return ShuttleBusesArrivalTimeResponse.builder()
                 .busName(shuttleBus.getBusName())
+                .isRunning(isRunning)
                 .times(shuttleTimeList)
                 .build();
     }
