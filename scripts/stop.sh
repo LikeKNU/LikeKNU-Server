36e1b9ec271b31b053a7b13543db1e8cd04bd12a
jcw1031 <jcw001031@gmail.com>
2023-11-21 16:37:50 +0900

    hotfix: 학사일정 요일 locale 한국 설정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,8 +6,7 @@ STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR)
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NouFound" >> $STOP_LOG
-  who
+  echo "서비스 NotFound" >> $STOP_LOG
 else
   echo "서비스 종료 " >> $STOP_LOG
   kill -9 "$SERVICE_PID"
