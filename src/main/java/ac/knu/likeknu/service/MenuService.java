59afc45d0b02708ab4a5a073322dad031d572319
ahma0 <dsd932@naver.com>
2023-11-28 17:08:06 +0900

    Feat: menu 한번에 보내도록 수정

--- src/main/java/ac/knu/likeknu/service/MenuService.java
+++ src/main/java/ac/knu/likeknu/service/MenuService.java
@@ -16,7 +16,9 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -36,18 +38,14 @@ public class MenuService {
                 .collect(Collectors.toList());
     }
 
-    /**
-     * MealType의 요소들을 이용해 밤이나 새벽이 아닐 경우 MealListDto 리스트를 만듭니다.
-     *
-     * @param cafeteria
-     * @param date
-     *
-     * @return
-     */
-    private List<MealListDto> filterMealtypeAndCreateList(Cafeteria cafeteria, LocalDate date) {
+    private Map<LocalDate, List<MealListDto>> filterMealtypeAndCreateList(Cafeteria cafeteria, LocalDate date) {
         return Arrays.stream(MealType.values())
-                .map(mealType -> findRepositoryAndMapDto(mealType, cafeteria, date))
-                .collect(Collectors.toList());
+                .flatMap(mealType -> Stream.of(date, date.plusDays(1))
+                        .map(day -> findRepositoryAndMapDto(mealType, cafeteria, day)))
+                .collect(
+                        Collectors.groupingBy(MealListDto::getDate,
+                                Collectors.mapping(mealListDto -> mealListDto, Collectors.toList())
+                        ));
     }
 
     /**
@@ -56,13 +54,12 @@ public class MenuService {
      * @param mealType
      * @param cafeteria
      * @param date
-     *
      * @return
      */
     private MealListDto findRepositoryAndMapDto(MealType mealType, Cafeteria cafeteria, LocalDate date) {
         return menuRepository.findByMenuDateAndCafeteriaAndMealType(date, cafeteria, mealType)
-                .map(menu -> MealListDto.of(mealType, cafeteria, menu.getMenus()))
-                .orElse(MealListDto.empty(mealType));
+                .map(menu -> MealListDto.of(mealType, cafeteria, menu))
+                .orElse(MealListDto.empty(mealType, date));
     }
 
 }
