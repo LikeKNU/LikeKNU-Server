274e80634b665f346c08d1b65877e2b3125e1b75
jcw1031 <jcw001031@gmail.com>
2024-06-02 17:33:07 +0900

    chore: configure logback pattern and appender by active profiles

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ec2-user/deploy"
-JAR="$ROOT_PATH/application.jar"
+ROOT_PATH="/home/ec2-user/LikeKNU"
+JAR="$ROOT_PATH/LikeKNU.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
@@ -9,12 +9,12 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/LikeKNU-1.0.0.jar $JAR
+echo "[$NOW] Copy $JAR" >> $START_LOG
+cp $ROOT_PATH/build/libs/*.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
+echo "[$NOW] Run $JAR" >> $START_LOG
 source ~/.bash_profile
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR --spring.profiles.active=prod > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+echo "[$NOW] Application PID: $SERVICE_PID" >> $START_LOG
