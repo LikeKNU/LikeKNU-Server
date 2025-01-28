0242ae2f05450bfc92c4e94affb10c4dcdce63ea
jcw1031 <jcw001031@gmail.com>
2025-01-28 11:28:55 +0900

    fix: 버스 도착시간 메시지 결정 로직 수정

--- src/main/java/ac/knu/likeknu/controller/dto/citybus/CityBusesArrivalTimeResponse.java
+++ src/main/java/ac/knu/likeknu/controller/dto/citybus/CityBusesArrivalTimeResponse.java
@@ -12,12 +12,19 @@ public record CityBusesArrivalTimeResponse(String busNumber, String remainingTim
 
     public static CityBusesArrivalTimeResponse of(CityBus cityBus, LocalTime arrivalTime, LocalTime currentTime) {
         long remainingTime = Duration.between(currentTime, arrivalTime).toMinutes();
-        String remainingStatus = remainingTime <= 1 ? "곧 도착" : remainingTime + "분";
+        String remainingStatus = determineRemainingStatusMessage(remainingTime);
         return CityBusesArrivalTimeResponse.builder()
                 .busNumber(cityBus.getBusNumber())
                 .remainingTime(remainingStatus)
                 .busColor(cityBus.getBusColor())
                 .arrivalAt(arrivalTime)
                 .build();
+    }
+
+    private static String determineRemainingStatusMessage(long remainingTime) {
+        if (remainingTime < 0) {
+            return null;
+        }
+        return remainingTime <= 1 ? "곧 도착" : remainingTime + "분";
     }
 }
