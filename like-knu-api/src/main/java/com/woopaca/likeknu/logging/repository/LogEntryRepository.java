22678a087db0c34b61250db6e993bfe7d962b9d5
jcw1031 <jcw001031@gmail.com>
2025-05-21 23:18:47 +0900

    불필요한 log 설정 제거 및 local-dev 프로필 추가

--- like-knu-api/src/main/java/com/woopaca/likeknu/logging/repository/LogEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.woopaca.likeknu.logging.repository;
-
-import com.woopaca.likeknu.logging.domain.LogEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogEntryRepository extends JpaRepository<LogEntry, Long> {
-}
--- like-knu-api/src/main/java/com/woopaca/likeknu/logging/repository/UserLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.woopaca.likeknu.logging.repository;
-
-import com.woopaca.likeknu.logging.domain.UserLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLogRepository extends JpaRepository<UserLog, Long> {
-}
